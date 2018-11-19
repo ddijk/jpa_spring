c859d62f31cbc5e27751d83cf519a15e4f1eeb81
ddijk <dick@dijkrosoft.nl>
2018-11-19 10:57:19 +0100

    Refactor script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,9 +4,9 @@ insert into person (name, age) values ( 'patricia', 40);
 insert into plugin (name) values ('osiris');
 insert into plugin (name) values ('trajectplanner');
 
-insert into config (plugin_id, name, value) values (1, 'url','http://xyz');
-insert into config (plugin_id, name, value) values (1, 'username','aap');
+insert into config (plugin_id, name, value) values ((select id from plugin where name='osiris'), 'url','http://xyz');
+insert into config (plugin_id, name, value) values ((select id from plugin where name='osiris'),, 'username','aap');
 
-insert into config (plugin_id, name, value) values (2, 'frankrijk','parijs');
-insert into config (plugin_id, name, value) values (2, 'nederland','amsterdam');
-insert into config (plugin_id, name, value) values (2, 'engeland','london');
+insert into config (plugin_id, name, value) values ((select id from plugin where name='trajectplanner'), 'frankrijk','parijs');
+insert into config (plugin_id, name, value) values ((select id from plugin where name='trajectplanner'), 'nederland','amsterdam');
+insert into config (plugin_id, name, value) values ((select id from plugin where name='trajectplanner'), 'engeland','london');
